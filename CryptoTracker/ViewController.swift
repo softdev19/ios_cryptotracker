b83d9a9f787cf4138081d0db3afb9eed0b90546a
collinrentz7 <collinrentz2@gmail.com>
2022-01-31 21:47:45 -0600

    Initial Commit

---
+++ CryptoTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by Uriel Hernandez Gonzalez on 31/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
