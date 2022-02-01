07937b34a515ef1e8b3fe0aed8ac3be0c628d14b
collinrentz7 <collinrentz2@gmail.com>
2022-01-31 22:05:05 -0600

    initial setup

--- CryptoTracker/Controllers/Home/HomeViewController.swift
+++ CryptoTracker/Controllers/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CryptoTracker
 //
 //  Created by Uriel Hernandez Gonzalez on 31/01/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
 }
-
