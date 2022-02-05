9de9c85ea1e77ad1f4fae0e43f2d4dcd404fc9ef
collinrentz7 <collinrentz2@gmail.com>
2022-02-04 20:50:46 -0600

    implemented basic home UI

--- CryptoTracker/Controllers/Tabs/HomeViewController.swift
+++ CryptoTracker/Controllers/Tabs/HomeViewController.swift
@@ -6,11 +6,22 @@
 //
 
 import UIKit
+import MaterialComponents.MaterialCards
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var totalCoinsCard: MDCCard!
+    @IBOutlet weak var marketsCard: MDCCard!
+    @IBOutlet weak var exchangesCard: MDCCard!
+    @IBOutlet weak var marketCapCard: MDCCard!
+    @IBOutlet weak var lastDayCard: MDCCard!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCards()
+        setupTable()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -19,4 +30,32 @@ class HomeViewController: UIViewController {
     }
 
 
+    private func setupCards(){
+        for card in [totalCoinsCard, marketsCard, exchangesCard, marketCapCard, lastDayCard] {
+            card?.setShadowColor(UIColor(named: "TextColor"), for: .normal)
+            card?.setShadowElevation(ShadowElevation(rawValue: 10), for: .normal)
+            card?.cornerRadius = 20
+        }
+    }
+    
+    private func setupTable(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+}
+
+//MARK: - Extensions
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //TODO: Table Cell
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //TODO: Return length of exchanges array
+        return 0
+    }
 }
