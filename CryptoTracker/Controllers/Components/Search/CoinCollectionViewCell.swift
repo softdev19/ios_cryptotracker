1fa7cb11bc8cbdc008c4bc22d8ee443ce232eef7
collinrentz7 <collinrentz2@gmail.com>
2022-02-06 13:57:26 -0600

    changed orientation params

---
+++ CryptoTracker/Controllers/Components/Search/CoinCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CoinCollectionViewCell.swift
+//  CryptoTracker
+//
+//  Created by Uriel Hernandez Gonzalez on 06/02/22.
+//
+
+import UIKit
+
+class CoinCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
