d6b582b77731ba95f315a78c8b7e46ce92575be2
collinrentz7 <collinrentz2@gmail.com>
2022-02-16 20:47:23 -0600

    displayed coin data on search vc

--- CryptoTracker/Controllers/Components/Search/CoinCollectionViewCell.swift
+++ CryptoTracker/Controllers/Components/Search/CoinCollectionViewCell.swift
@@ -18,8 +18,11 @@ class CoinCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var cardView: MDCCard!
     @IBOutlet weak var gradientView: UIView!
+    @IBOutlet weak var coinImage: UIImageView!
+    @IBOutlet weak var coinNameLbl: UILabel!
     
     public var delegate: CoinCollectionViewCellDelegate?
+    public var coindId: String!
 
     private var gradient: CAGradientLayer = {
         let gradient = CAGradientLayer()
@@ -34,16 +37,33 @@ class CoinCollectionViewCell: UICollectionViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupUI()
+    }
+
+    private func setupUI() {
         cardView.layer.masksToBounds = true
         cardView.cornerRadius = 20
+        coinImage.layer.cornerRadius = coinImage.bounds.height / 2
         gradient.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
         gradientView.layer.insertSublayer(gradient, at: 0)
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap))
         gradientView.addGestureRecognizer(tapGesture)
     }
 
+    func displayData(name: String?, imgUrl: String?) {
+
+        guard let name = name,
+              let imgUrl = imgUrl,
+              let url = URL(string: imgUrl) else {
+                  return
+              }
+
+        coinNameLbl.text = name
+        coinImage.sd_setImage(with: url)
+    }
+
     @objc private func didTap() {
-        delegate?.onClicked(coindId: "coinId")
+        delegate?.onClicked(coindId: coindId)
     }
 
 }
--- CryptoTracker/Controllers/Tabs/SearchViewController.swift
+++ CryptoTracker/Controllers/Tabs/SearchViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MaterialComponents.MDCOutlinedTextField
+import SkeletonView
 
 class SearchViewController: UIViewController {
 
@@ -28,9 +29,18 @@ class SearchViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.topViewController?.title = "Search a Coin"
+        showSkeletonView()
         DispatchQueue.global(qos: .userInitiated).async {
             self.fetchCoins()
         }
+    }
+
+    private func showSkeletonView() {
+        collectionView.showGradientSkeleton(usingGradient: .init(baseColor: UIColor(named: "MainColor")!,
+                                                                 secondaryColor: UIColor(named: "SecondaryColor")!),
+                                                                 animated: true,
+                                                                 delay: 0,
+                                                                 transition: .crossDissolve(0.25))
     }
 
     private func setupSearchField() {
@@ -64,7 +74,7 @@ class SearchViewController: UIViewController {
 
             DispatchQueue.main.async {
                 self.coins = coinList.data!.coins
-                self.collectionView.reloadData()
+                self.collectionView.hideSkeleton(reloadDataAfter: true, transition: .crossDissolve(0.2))
             }
         }
     }
@@ -72,8 +82,12 @@ class SearchViewController: UIViewController {
 
 // MARK: - Extensions
 extension SearchViewController: UICollectionViewDelegate,
-                                    UICollectionViewDataSource,
-                                    UICollectionViewDelegateFlowLayout {
+                                    SkeletonCollectionViewDataSource,
+                                UICollectionViewDelegateFlowLayout {
+
+    func collectionSkeletonView(_ skeletonView: UICollectionView, cellIdentifierForItemAt indexPath: IndexPath) -> ReusableCellIdentifier {
+        return CoinCollectionViewCell.identifier
+    }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return coins.count
@@ -84,6 +98,8 @@ extension SearchViewController: UICollectionViewDelegate,
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CoinCollectionViewCell.identifier,
                                                       for: indexPath) as? CoinCollectionViewCell
         cell?.delegate = self
+        cell?.coindId = coins[indexPath.row].uuid!
+        cell?.displayData(name: coins[indexPath.row].name, imgUrl: coins[indexPath.row].iconUrl)
         return cell!
     }
 
@@ -101,6 +117,4 @@ extension SearchViewController: CoinCollectionViewCellDelegate {
     func onClicked(coindId: String) {
         // TODO: Redirect to other view
     }
-    
-    
 }
